a35ace1be855b7e48a0220c4a943824a1b6f26d4
U <ut846512@outlook.com>
2022-08-30 02:40:29 +0300

    preview2

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -32,18 +32,7 @@ export const BurgerIngredients = ({ data, openIngredientDetails }) => {
         <div className="p-4">
           <p className="text text_type_main-large">Соберите бургер</p>
         </div>
-        <div style={{ display: "flex" }}>
-          {/* <Tab value="one" active={current === "one"} onClick={setCurrent}>
-            One
-          </Tab>
-          <Tab value="two" active={current === "two"} onClick={setCurrent}>
-            Two
-          </Tab>
-          <Tab value="three" active={current === "three"} onClick={setCurrent}>
-            Three
-          </Tab> */}
-          {/* {tabs} */}
-        </div>
+        <div style={{ display: "flex" }}>{tabs}</div>
       </div>
       <div className={styles.box}>
         <div className="m-2">
